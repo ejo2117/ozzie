850f9525de5202a92bb29bfeab53803ec2313a40
Ethan O'Neal <35907890+ejo2117@users.noreply.github.com>
2022-09-29 16:08:55 -0400

    Sticker layer (#4)
    * Dark Mode Theme Color
    
    * Add Sticker Container

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import 'variables.scss';`,
+	},
+	swcMinify: true,
+};
+
+module.exports = nextConfig;
