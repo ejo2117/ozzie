62b24f1433f6dfedb1e676f9245b769cdd5d4fe1
Ethan <ejo2117@columbia.edu>
2022-11-30 19:45:20 -0500

    Add spotify libfile

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,18 +1,63 @@
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 
-const requestUserAuthorization = () => {
-	const scope = '';
+// const requestUserAuthorization = () => {
+// 	const scope = '';
 
-	const authParams = {
-		client_id: '',
-		response_type: 'code',
-		redirect_uri: 'http://localhost:3000',
-		state: '',
-		scope: '',
-		// PKCE
-		code_challenge_method: '',
-		code_challenge: '',
-	};
+// 	const authParams = {
+// 		client_id: '',
+// 		response_type: 'code',
+// 		redirect_uri: 'http://localhost:3000',
+// 		state: '',
+// 		scope: '',
+// 		// PKCE
+// 		code_challenge_method: '',
+// 		code_challenge: '',
+// 	};
+// };
+
+// export { requestUserAuthorization };
+
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+
+const basic = btoa(`${client_id}:${client_secret}`);
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+const getAccessToken = async () => {
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token
+    })
+  });
+
+  return response.json();
 };
 
-export { requestUserAuthorization };
+export const getNowPlaying = async () => {
+  const { access_token } = await getAccessToken();
+
+  return fetch(NOW_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`
+    }
+  });
+};
+
+export const getTopTracks = async () => {
+  const { access_token } = await getAccessToken();
+
+  return fetch(TOP_TRACKS_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`
+    }
+  });
+};
