cf6f30392562be76424594527d6c099b2ad7809c
Ethan <ejo2117@columbia.edu>
2022-12-03 19:50:57 -0500

    Update theming

--- components/Screen/Field.tsx
+++ components/Screen/Field.tsx
@@ -1,69 +1,74 @@
-import { useCallback, useEffect, useRef, useState } from "react"
-import * as d3 from 'd3'
-import Sprite from "./Sprite"
-import { Position } from "@lib/types"
+import { useCallback, useEffect, useRef, useState } from 'react';
+import * as d3 from 'd3';
+import Sprite from './Sprite';
+import { Position } from '@lib/types';
 
+const getUserTheme = () => {
+	if (typeof document === 'undefined') {
+		return 'l';
+	}
+	return getComputedStyle(document.body, ':after').content;
+};
 
 const Field = () => {
-    // Hooks
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-    const [csvData, setCsvData] = useState()
-    const [context, setContext] = useState<CanvasRenderingContext2D>()
-    const [sprites, setSprites] = useState<Sprite[]>()
-    
-    // Set Context on Mount
-    useEffect(() => {
-        if(canvasRef.current) { 
-            canvasRef.current.height = window.innerHeight
-            canvasRef.current.width = window.innerWidth
-            setContext(canvasRef.current.getContext("2d")!) 
-        }
-    }, [canvasRef.current])
+	// Hooks
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [csvData, setCsvData] = useState();
+	const [context, setContext] = useState<CanvasRenderingContext2D>();
+	const [sprites, setSprites] = useState<Sprite[]>();
 
-    // Create Sprites 
-    useEffect(() => {
-        if (context) {
-            const results = []
-            const createdAt = performance.now()
-            for (let i = 0; i < 1; i++) {
-                results.push(new Sprite({context, weight: 4, bpm: 120, created: createdAt}))
-            }
-            setSprites(results)
-        }
-    }, [context])
+	// Set Context on Mount
+	useEffect(() => {
+		if (canvasRef.current) {
+			canvasRef.current.height = window.innerHeight;
+			canvasRef.current.width = window.innerWidth;
+			setContext(canvasRef.current.getContext('2d')!);
+		}
+	}, [canvasRef.current]);
 
-    // Logic
-    const update = useCallback(()=>{
-        if(!context || !sprites) return
-        
-        let now: number, sprite: Sprite;
-  
-        context.fillStyle = "#f9f9f9";
-        // Wipes Canvas on each update
-        context.fillRect(0, 0, window.innerWidth, window.innerHeight);
-        // context.save()
+	// Create Sprites
+	useEffect(() => {
+		if (context) {
+			const results = [];
+			const createdAt = performance.now();
+			for (let i = 0; i < 1; i++) {
+				results.push(new Sprite({ context, weight: 4, bpm: 120, created: createdAt }));
+			}
+			setSprites(results);
+		}
+	}, [context]);
 
-        // put aside so all sprites are drawn for the same ms
-        now = performance.now(); 
-        
-        for (sprite of sprites)
-        {
-            const head = sprite.render(sprite.move((now - sprite.created) / 1000));
-            
-            // Debug movement head 
-            console.log([Math.floor(head[0]), Math.floor(head[1])])
-        }
+	// Logic
+	const update = useCallback(() => {
+		if (!context || !sprites) return;
 
-        window.requestAnimationFrame(() => update());
-    },[context, sprites])
+		let now: number, sprite: Sprite;
 
-    try {
-        window.requestAnimationFrame(() => update())
-    } catch (error) {
-        // assume window is not available
-    }
+		context.fillStyle = getUserTheme().includes('l') ? '#fff' : '#000';
+		// Wipes Canvas on each update
+		context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+		// context.save()
 
-    return <canvas ref={canvasRef}></canvas>
-}
+		// put aside so all sprites are drawn for the same ms
+		now = performance.now();
 
-export default Field
+		for (sprite of sprites) {
+			const head = sprite.render(sprite.move((now - sprite.created) / 1000));
+
+			// Debug movement head
+			// console.log([Math.floor(head[0]), Math.floor(head[1])]);
+		}
+
+		window.requestAnimationFrame(() => update());
+	}, [context, sprites]);
+
+	try {
+		window.requestAnimationFrame(() => update());
+	} catch (error) {
+		// assume window is not available
+	}
+
+	return <canvas ref={canvasRef}></canvas>;
+};
+
+export default Field;
